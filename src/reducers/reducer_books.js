172d96fd8df7f74ddc4597dae66058974bef6a96
Juan Salvatore <juansalvatore@live.com.ar>
2017-12-10 19:14:15 -0300

    Practice project

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+// books reducer
+export default function() {
+    return [
+        {title: 'Javascript: The Good Parts', pages: 101},
+        {title: 'Harry Potter', pages: 222},
+        {title: 'The Dark Tower', pages: 400},
+        {title: 'Eloquent Ruby', pages: 322},
+    ]
+}
